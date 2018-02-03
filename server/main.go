62f2c554c284c353ca35067a762169ed06835b8f
astenmies <s.dolbecke@gmail.com>
2018-02-03 15:56:18 +0100

    Context work

--- server/main.go
+++ server/main.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"net/http"
 
+	"fmt"
 	"io/ioutil"
 
-	"fmt"
+	context "./context"
 
 	"github.com/neelance/graphql-go"
 	"github.com/neelance/graphql-go/relay"
@@ -40,21 +41,23 @@ func init() {
 }
 
 func main() {
+
+	ctx := context.CustomContext()
+
 	// We will start a small server that reads our "graphiql.html" file and
 	// responds with it, so we are able to have our own graphiql
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(page)
+		http.ServeFile(w, r, "graphiql.html")
 	}))
 
 	http.Handle("/query", &relay.Handler{Schema: schema})
 
 	// This is where our graphql server is handled, we declare "/graphql" as the route
 	// where all our graphql requests will be directed to
-	// http.Handle("/graphql", &relay.Handler{Schema: schema})
 	http.Handle("/graphql", cors.Default().Handler(&relay.Handler{Schema: schema}))
 
 	// We start the server by using ListenAndServe and we log if we have any error, hope not!
-	fmt.Println("Listening at http://localhost:8080")
+	fmt.Println(" listening at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -66,40 +69,3 @@ type Resolver struct{}
 func (r *Resolver) Hello() string {
 	return "world"
 }
-
-var page = []byte(`
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.10.2/graphiql.css" />
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/1.1.0/fetch.min.js"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react.min.js"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react-dom.min.js"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.10.2/graphiql.js"></script>
-		</head>
-		<body style="width: 100%; height: 100%; margin: 0; overflow: hidden;">
-			<div id="graphiql" style="height: 100vh;">Loading...</div>
-			<script>
-				function graphQLFetcher(graphQLParams) {
-					return fetch("/query", {
-						method: "post",
-						body: JSON.stringify(graphQLParams),
-						credentials: "include",
-					}).then(function (response) {
-						return response.text();
-					}).then(function (responseBody) {
-						try {
-							return JSON.parse(responseBody);
-						} catch (error) {
-							return responseBody;
-						}
-					});
-				}
-				ReactDOM.render(
-					React.createElement(GraphiQL, {fetcher: graphQLFetcher}),
-					document.getElementById("graphiql")
-				);
-			</script>
-		</body>
-	</html>
-	`)
