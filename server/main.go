34084db66750a057615baaff28254ce9bf7631da
astenmies <s.dolbecke@gmail.com>
2018-02-04 00:15:24 +0100

    Graphql stuff

--- server/main.go
+++ server/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	"fmt"
-	"io/ioutil"
-
-	"github.com/neelance/graphql-go"
-	"github.com/neelance/graphql-go/relay"
+	blapi "./blapi"
+	handler "./handler"
+	loader "./loader"
+	resolver "./resolver"
+	schema "./schema"
+	graphql "github.com/neelance/graphql-go"
 	"github.com/rs/cors"
+	"github.com/spf13/viper"
 )
-
-// This will be use by our handler at /graphql
-var schema *graphql.Schema
 
 // Documentation on how to print http requests
 // https://medium.com/doing-things-right/pretty-printing-http-requests-in-golang-a918d5aaa000
@@ -21,24 +21,35 @@ var schema *graphql.Schema
 // This function runs at the start of the program
 func init() {
 
-	// We get the schema from the file, rather than having the schema inline here
-	// I think will lead to better organizaiton of our own code
-	schemaFile, err := ioutil.ReadFile("schema.graphql")
+	// Initialize viper
+	// After this, we can call viper.Get("string") anywhere
+	viper.SetConfigName("Config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
-		// We will panic if we don't find the schema.graphql file in our server
-		panic(err)
-	}
-
-	// We will use graphql-go library to parse our schema from "schema.graphql"
-	// and the resolver is our struct that should fullfill everything in the Query
-	// from our schema
-	schema, err = graphql.ParseSchema(string(schemaFile), &Resolver{})
-	if err != nil {
-		panic(err)
+		log.Fatalf("Fatal error config file: %s \n", err)
 	}
 }
 
 func main() {
+
+	var (
+		appName = viper.Get("app-name").(string)
+	)
+
+	c := blapi.NewClient(http.DefaultClient) // TODO: don't use the default client.
+
+	root, err := resolver.NewRoot(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the request handler; inject dependencies.
+	h := handler.GraphQL{
+		// Parse and validate schema. Panic if unable to do so.
+		Schema:  graphql.MustParseSchema(schema.String(), root),
+		Loaders: loader.Initialize(c),
+	}
 
 	// We will start a small server that reads our "graphiql.html" file and
 	// responds with it, so we are able to have our own graphiql
@@ -46,22 +57,12 @@ func main() {
 		http.ServeFile(w, r, "graphiql.html")
 	}))
 
-	http.Handle("/query", &relay.Handler{Schema: schema})
+	http.Handle("/query", h)
 
 	// This is where our graphql server is handled, we declare "/graphql" as the route
 	// where all our graphql requests will be directed to
-	http.Handle("/graphql", cors.Default().Handler(&relay.Handler{Schema: schema}))
-
+	http.Handle("/graphql", cors.Default().Handler(h))
 	// We start the server by using ListenAndServe and we log if we have any error, hope not!
-	fmt.Println(" listening at http://localhost:8080")
+	fmt.Println(appName, "listening at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-// Resolver struct is the main resolver  wich we will use to fullfill
-// queries and mutations that our schema.graphql defines
-type Resolver struct{}
-
-// Hello function resolves to hello: String! in the Query object in our schema.graphql
-func (r *Resolver) Hello() string {
-	return "world"
 }
