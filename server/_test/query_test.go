251f7c210f6f5b6563671d23de17a2066cac03e2
astenmies <hello@asten.io>
2018-03-25 01:20:01 +0100

    Test

--- server/_test/query_test.go
+++ server/_test/query_test.go
@@ -1,10 +1,12 @@
 package test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/astenmies/blackari/server/gqlResolver"
 	"github.com/astenmies/blackari/server/gqlSchema"
+	"github.com/astenmies/blackari/server/mongo"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/gqltesting"
 )
@@ -20,9 +22,16 @@ func TestQuery(t *testing.T) {
 var rootSchema = graphql.MustParseSchema(gqlSchema.GetRootSchema(), &gqlResolver.Resolver{})
 
 func TestPost(t *testing.T) {
+	ctx := context.Background()
+	// Call mongo Get, session and reference to the post collection
+	session, collection := mongo.Get("post")
+	// Close the session so its resources may be put back in the pool or collected, depending on the case.
+	defer session.Close()
+	con := context.WithValue(ctx, "collection", collection)
 	gqltesting.RunTests(t, []*gqltesting.Test{
 		{
-			Schema: rootSchema,
+			Context: con,
+			Schema:  rootSchema,
 			Query: `
 				{
 					post(slug:"second-post") {
